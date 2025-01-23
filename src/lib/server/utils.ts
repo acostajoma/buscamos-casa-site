1cecfd7cbe703ab51d9cc8669092c852edce76bb
Martin Acosta <acostajoma9@gmail.com>
2025-01-23 14:11:09 -0600

    Adding util scripts and fixing env var issue

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -1,4 +1,4 @@
-import { CLOUDINARY_API_SECRET } from '$env/dynamic/private';
+import { env } from '$env/dynamic/private';
 import { sha1 } from '@oslojs/crypto/sha1';
 
 export const getCloudinarySignature = (paramsToSign: Cloudinary.ParamsToSign) => {
@@ -7,7 +7,7 @@ export const getCloudinarySignature = (paramsToSign: Cloudinary.ParamsToSign) =>
 		.map((key) => `${key}=${paramsToSign[key as keyof Cloudinary.ParamsToSign]}`)
 		.join('&');
 
-	const stringToSign = `${paramString}${CLOUDINARY_API_SECRET}`;
+	const stringToSign = `${paramString}${env.CLOUDINARY_API_SECRET}`;
 	const hashedString = sha1(new TextEncoder().encode(stringToSign));
 	const signature = Array.from(new Uint8Array(hashedString))
 		.map((b) => b.toString(16).padStart(2, '0'))
