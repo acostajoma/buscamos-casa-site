e9b9de8b581757dc64c0765a0e927506623401c5
Martin Acosta <acostajoma9@gmail.com>
2025-02-13 14:50:07 -0600

    Adding new breakpoints and configuring Grid for better looking aspect

--- src/lib/components/grid/Grid.svelte
+++ src/lib/components/grid/Grid.svelte
@@ -7,6 +7,6 @@
 	let { children }: Props = $props();
 </script>
 
-<div class="grid grid-cols-2 gap-px overflow-hidden rounded-lg sm:grid-cols-3">
+<div class="grid grid-cols-1 gap-px overflow-hidden rounded-lg 2xs:grid-cols-2 md:grid-cols-3">
 	{@render children()}
 </div>
