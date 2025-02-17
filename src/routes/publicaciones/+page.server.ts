5c7704315279fb760c767df57a6a74912919627d
Martin Acosta <acostajoma9@gmail.com>
2025-02-17 17:52:23 -0600

    Adding cache functionality

--- src/routes/publicaciones/+page.server.ts
+++ src/routes/publicaciones/+page.server.ts
@@ -2,9 +2,16 @@ import { getPosts } from '$lib/server/utils';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url, locals }) => {
-	const { db } = locals;
+	const { db, cache } = locals;
 	const { searchParams } = url;
 	const pageNumber = searchParams.get('page');
+	const cacheKey = url.pathname + url.search;
+	const cachedPosts = await cache.get(cacheKey, 'json');
+	if (cachedPosts) {
+		return cachedPosts;
+	}
 	const { postCount, posts } = await getPosts(db, pageNumber);
+	await cache.put(cacheKey, JSON.stringify({ postCount, posts }), { expirationTtl: 60 });
+
 	return { postCount, posts };
 };
