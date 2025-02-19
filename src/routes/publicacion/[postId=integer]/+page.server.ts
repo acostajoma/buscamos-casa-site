cdb43558810f389b1bd7ec243834d9d4714178cb
Martin Acosta <acostajoma9@gmail.com>
2025-02-19 14:44:48 -0600

    Fixing issue with empty page on postid page

--- src/routes/publicacion/[postId=integer]/+page.server.ts
+++ src/routes/publicacion/[postId=integer]/+page.server.ts
@@ -29,13 +29,14 @@ export const load: PageServerLoad = async ({ params, locals, setHeaders, url }) 
 
 	const cacheKey = url.pathname;
 	const cachedPost = await cache.get(cacheKey, 'json');
-	setHeaders({ 'Cache-Control': 'public, max-age=300' });
-	if (cachedPost) {
-		return cachedPost as Awaited<ReturnType<typeof getPosts>>;
-	}
+	let post: Awaited<ReturnType<typeof getPosts>>;
 
-	const post = await getPosts(db, params);
-	await cache.put(cacheKey, JSON.stringify(post), { expirationTtl: 300 });
+	if (cachedPost) {
+		post = cachedPost as Awaited<ReturnType<typeof getPosts>>;
+	} else {
+		post = await getPosts(db, params);
+		await cache.put(cacheKey, JSON.stringify(post), { expirationTtl: 300 });
+	}
 
 	// Quick trick to set listing status to 'Publicado' for testing purposes
 	if (dev) {
@@ -64,6 +65,8 @@ export const load: PageServerLoad = async ({ params, locals, setHeaders, url }) 
 	if (post.listingStatus === 'Alquilado' || post.listingStatus === 'Vendido') {
 		error(406, 'Esta propiedad ha sido marcada como vendida o alquilada.');
 	}
+
+	setHeaders({ 'Cache-Control': 'public, max-age=300' });
 
 	return {
 		post
