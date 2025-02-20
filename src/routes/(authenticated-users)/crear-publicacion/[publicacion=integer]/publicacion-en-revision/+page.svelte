532fd636dadd5d2adc12843fe05cbcdf9c469e8d
Martin Acosta <acostajoma9@gmail.com>
2025-02-20 16:14:19 -0600

    Making use of Hero plus small change on component

--- src/routes/(authenticated-users)/crear-publicacion/[publicacion=integer]/publicacion-en-revision/+page.svelte
+++ src/routes/(authenticated-users)/crear-publicacion/[publicacion=integer]/publicacion-en-revision/+page.svelte
@@ -1,45 +1,25 @@
+<script>
+	import Hero from '$lib/components/Hero.svelte';
+</script>
+
 <svelte:head>
 	<title>Publicación en revisión</title>
 </svelte:head>
 
-<div class="relative my-32 px-6 lg:px-8">
-	<div
-		class="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 transform-gpu justify-center overflow-hidden blur-3xl sm:top-auto sm:right-[calc(50%-6rem)] sm:bottom-0 sm:translate-y-0 sm:transform-gpu sm:justify-end"
-		aria-hidden="true"
-	>
-		<div
-			class="aspect-1108/632 w-[69.25rem] flex-none bg-linear-to-r from-[#FFD700] to-[#9089fc] opacity-25"
-			style="clip-path: polygon(73.6% 48.6%, 91.7% 88.5%, 100% 53.9%, 97.4% 18.1%, 92.5% 15.4%, 75.7% 36.3%, 55.3% 52.8%, 46.5% 50.9%, 45% 37.4%, 50.3% 13.1%, 21.3% 36.2%, 0.1% 0.1%, 5.4% 49.1%, 21.4% 36.4%, 58.9% 100%, 73.6% 48.6%)"
-		></div>
-	</div>
-	<div class="mx-auto max-w-2xl text-center">
-		<h2 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
-			Tu publicación está en revisión.
-		</h2>
-		<p class="mx-auto mt-6 max-w-xl text-lg/8 text-pretty text-gray-600">
-			Tu publicación será revisada por nuestro equipo para asegurarnos de que cumple con nuestras
+<Hero
+	headline="Tu publicación está en revisión."
+	description="Tu publicación será revisada por nuestro equipo para asegurarnos de que cumple con nuestras
 			políticas de contenido. Podrás verificar el resultado de la revisión en tu panel de
-			publicaciones. Toma en cuenta que este proceso puede tardar hasta 24 horas.
-		</p>
-		<div class="mt-10 flex items-center justify-center gap-x-6">
-			<a
-				href="/"
-				class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-				>Ir a inicio</a
-			>
-			<!-- @TODO CREAR ESTE LINK  -->
-			<a href="/nuestras-politicas" class="text-sm/6 font-semibold text-gray-900"
-				>Nuestras políticas<span aria-hidden="true">→</span></a
-			>
-		</div>
-	</div>
-	<div
-		class="absolute top-full right-0 left-1/2 -z-10 hidden -translate-y-1/2 transform-gpu overflow-hidden blur-3xl sm:block"
-		aria-hidden="true"
-	>
-		<div
-			class="aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#FFD700] to-[#9089fc] opacity-30"
-			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-		></div>
-	</div>
-</div>
+			publicaciones. Toma en cuenta que este proceso puede tardar hasta 24 horas."
+>
+	{#snippet bottomZone()}
+		<a
+			href="/"
+			class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+			>Ir a la página de inicio</a
+		>
+		<a href="/terminos-y-condiciones" class="text-sm/6 font-semibold text-gray-900"
+			>Nuestras Políticas<span aria-hidden="true">→</span></a
+		>
+	{/snippet}
+</Hero>
