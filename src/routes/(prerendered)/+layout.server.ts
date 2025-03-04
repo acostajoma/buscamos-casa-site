b770f5afe17e838c18f22f9d52b54bbd85313413
Martin Acosta <acostajoma9@gmail.com>
2025-03-04 13:05:30 -0600

    Setting prerender to false

--- src/routes/(prerendered)/+layout.server.ts
+++ src/routes/(prerendered)/+layout.server.ts
@@ -1,1 +1,3 @@
-export const prerender = 'auto';
+// this routes are prerenderable but are not prerendered by default because of problems with Sentry
+// We might cache its content somehow and then prerender it
+export const prerender = false;
