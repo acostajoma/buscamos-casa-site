4dd3d1a4afa935f488e805d89f5db3a315f7bb5f
Martin Acosta <acostajoma9@gmail.com>
2025-03-07 14:32:14 -0600

    changing dev quota

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,12 +1,12 @@
 import { dev } from '$app/environment';
-import { PUBLIC_ENVIRONMENT } from '$env/static/public';
+import { PUBLIC_ENVIRONMENT, PUBLIC_SENTRY_DSN } from '$env/static/public';
 import * as Sentry from '@sentry/sveltekit';
 import { handleErrorWithSentry } from '@sentry/sveltekit';
 
 Sentry.init({
-	dsn: 'https://91e99e7f2e932483e521c8aa159affb3@o4508807636123648.ingest.us.sentry.io/4508807637696512',
+	dsn: PUBLIC_SENTRY_DSN,
 	environment: PUBLIC_ENVIRONMENT,
-	tracesSampleRate: dev ? 1.0 : 0.2
+	tracesSampleRate: dev ? 0 : 0.2
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
